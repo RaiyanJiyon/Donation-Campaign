ddcbf7f0356d88a5b6c215ea1523214af0c45e97
RaiyanJiyon <raiyanjiyon814@gmail.com>
2024-11-11 14:18:56 +0600

    feat: update DonateCard component for dynamic styling
    - Implemented inline styles for dynamic background and text colors
    - Ensured Tailwind CSS compatibility by removing dynamic class names

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -6,13 +6,13 @@ const Banner = () => {
     <div
       className="w-11/12 mx-auto mt-6 hero"
       style={{
-        backgroundImage: `url(${bannerImage})`,
+        backgroundImage: `url(https://img.freepik.com/free-photo/happy-volunteers-hugging-each-other-while-preparing-donation-boxes_23-2148732640.jpg?t=st=1731312145~exp=1731315745~hmac=97c1a6cfa842a1b6dc759678c688e251a108f99e0d8c4f3c348b0de9cf9f52b8&w=826)`,
         backgroundSize: "cover",
         backgroundPosition: "center",
         backgroundRepeat: "no-repeat",
       }}
     >
-      <div className="hero-overlay bg-opacity-80"></div>
+      <div className="hero-overlay bg-opacity-60"></div>
       <div className="hero-content text-neutral-content text-center">
         <div className="max-w-md pt-20 pb-20">
           <h1 className="mb-5 text-5xl font-bold">
--- src/components/DonateCard/DonateCard.jsx
+++ src/components/DonateCard/DonateCard.jsx
@@ -5,7 +5,7 @@ const DonateCard = ({ data }) => {
     const {id, picture, title, category, category_bg, card_bg, text_button_bg, description, price} = data;
 
     return (
-        <div className="card card-compact rounded-lg shadow-lg">
+        <div className="card card-compact rounded-lg shadow-lg" style={{backgroundColor: card_bg}}>
             <figure>
                 <img
                 className='w-[312px], h-[194px]'
@@ -13,8 +13,8 @@ const DonateCard = ({ data }) => {
                     alt={`${picture} image`} />
             </figure>
             <div className="card-body">
-            <button className="btn btn-sm w-[40%]">{category}</button>
-                <h2 className="card-title">{title}</h2>
+            <button className="btn btn-sm w-[40%]" style={{backgroundColor: category_bg}}>{category}</button>
+                <h2 className="card-title" style={{color: text_button_bg}}>{title}</h2>
             </div>
         </div>
     );
