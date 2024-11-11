de8e2afd9e630edce69f5748d49ac555614c7ca9
RaiyanJiyon <raiyanjiyon814@gmail.com>
2024-11-11 20:19:52 +0600

    Added show all button functionality

--- src/pages/Donation.jsx
+++ src/pages/Donation.jsx
@@ -1,32 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { getToDonation } from '../util/addToDb';
-import { useLoaderData } from 'react-router-dom';
+import { Link, useLoaderData } from 'react-router-dom';
 
 const Donation = () => {
+    const [showAll, setShowAll] = useState(false);
     const donationDetailsId = getToDonation();
-
     const allDonation = useLoaderData();
-
     const filteredDonations = allDonation.filter(donation =>
         donationDetailsId.includes(donation.id)
     );
 
+    const donationsToShow = showAll ? filteredDonations : filteredDonations.slice(0, 4);
+
     return (
-        <div className='w-11/12 mx-auto mt-10 pb-10 grid lg:grid-cols-2 gap-6'>
+        <div className='w-11/12 mx-auto mt-10 pb-10 '>
+            <div className='grid lg:grid-cols-2 gap-6'>
+                {
+                    donationsToShow.map(donation => (
+                        <div key={donation.id} className='flex flex-col md:flex-row rounded-lg' style={{ backgroundColor: donation.card_bg }}>
+                            <div className='md:w-2/5'>
+                                <img className='w-full h-full rounded-l-lg' src={donation.picture} alt="" />
+                            </div>
+                            <div className='m-4 space-y-3'>
+                                <button className='btn btn-sm font-bold' style={{ backgroundColor: donation.category_bg, color: donation.text_button_bg }}>{donation.category}</button>
+                                <h2 className='text-2xl font-bold md:min-h-16'>{donation.title}</h2>
+                                <p className='text-sm font-semibold' style={{ color: donation.text_button_bg }}>${donation.price.toFixed(2)}</p>
+                                <Link className='btn font-bold text-white' style={{ backgroundColor: donation.category_bg }}>View Details</Link>
+                            </div>
+                        </div>
+                    ))
+                }
+            </div>
+
             {
-                filteredDonations.map(donation => (
-                    <div key={donation.id} className='flex flex-col md:flex-row rounded-lg' style={{ backgroundColor: donation.card_bg }}>
-                        <div className='md:w-2/5'>
-                            <img className='w-full h-full rounded-l-lg' src={donation.picture} alt="" />
-                        </div>
-                        <div className='m-4 space-y-3'>
-                            <button className='btn btn-sm font-bold' style={{ backgroundColor: donation.category_bg, color: donation.text_button_bg }}>{donation.category}</button>
-                            <h2 className='text-2xl font-bold md:min-h-16'>{donation.title}</h2>
-                            <p className='text-sm font-semibold' style={{ color: donation.text_button_bg }}>${donation.price.toFixed(2)}</p>
-                            <button className='btn font-bold text-white' style={{ backgroundColor: donation.category_bg }}>View Details</button>
-                        </div>
-                    </div>
-                ))
+                !showAll && filteredDonations.length > 4 &&
+                <div className='flex justify-center mt-10'>
+                    <button onClick={() => setShowAll(true)} className='btn bg-[#009444] text-white font-bold'>See All</button>
+                </div>
             }
         </div>
     );
